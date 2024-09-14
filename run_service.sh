11988ea51539bced7d9f76342a82068c8e5706f6
Joe Fernandez <joefernandez@google.com>
2024-09-14 17:30:06 +0000

    Updated run_service script
    - Added --host setting to allow other devices to connect

--- run_service.sh
+++ run_service.sh
@@ -6,4 +6,5 @@ source venv/bin/activate
 #python3 gemma_service_main.py
 
 cd gemma_service/
-uvicorn gemma_service_main:app --reload
+# to allow more than localhost access, add "--host 0.0.0.0":
+uvicorn gemma_service_main:app --host 0.0.0.0 --reload 
